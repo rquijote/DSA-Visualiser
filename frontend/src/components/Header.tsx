63db28098c30fb3972c7a9b31d6739a1e9483afc
Rino Miguel Sagario Quijote <rinomiguelq@gmail.com>
2025-11-16 16:36:26 +1100

    Small updates to the header, changing size and image.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import backBtn from '../assets/left-arrow.png'
 
 function Header({ title }: { title: string }) {
     return <div className="header">
-        <img src={backBtn} className='backBtn' alt="back button" title="Icon by Lizel Arina - Flaticon" />
+        <img src={backBtn} className='backBtn' alt="back button" title="Icon by Riconsly - Flaticon - https://www.flaticon.com/free-icons/left-button" />
         <h1>{title}</h1>
     </div> 
 }
