980989b48bc1b02a90aea12bc0f2d21ae3d9d993
Rino Miguel Sagario Quijote <rinomiguelq@gmail.com>
2025-11-20 17:10:27 +1100

    Changed to more minimalistic homepage.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,48 +4,48 @@ function App() {
   return (
     <div className="home-container">
       <div className="home-content-container">
-        <h1 className="main-title">Algorithms</h1>
+        <h1 className="main-title">Algorithm Visualiser</h1>
 
         <div className="home-section">
-          <h2 className="home-h2">Sorting Algorithms</h2>
+          <h2 className="home-h2">Sorting</h2>
           <div className="home-button-grid">
-            <NavLink className="home-alg-btn primary" to="/bubble-sort">
+            <NavLink className="home-alg-btn" to="/bubble-sort">
               Bubble Sort
             </NavLink>
-            <NavLink className="home-alg-btn secondary" to="/insertion-sort">
+            <NavLink className="home-alg-btn" to="/insertion-sort">
               Insertion Sort
             </NavLink>
-            <NavLink className="home-alg-btn tertiary" to="/selection-sort">
+            <NavLink className="home-alg-btn" to="/selection-sort">
               Selection Sort
             </NavLink>
-            <NavLink className="home-alg-btn quaternary" to="/merge-sort">
+            <NavLink className="home-alg-btn" to="/merge-sort">
               Merge Sort
             </NavLink>
-            <NavLink className="home-alg-btn primary" to="/quick-sort">
+            <NavLink className="home-alg-btn" to="/quick-sort">
               Quick Sort
             </NavLink>
           </div>
         </div>
 
         <div className="home-section">
-          <h2 className="home-h2">Searching Algorithms</h2>
+          <h2 className="home-h2">Searching</h2>
           <div className="home-button-grid">
-            <NavLink className="home-alg-btn secondary" to="/linear-search">
+            <NavLink className="home-alg-btn" to="/linear-search">
               Linear Search
             </NavLink>
-            <NavLink className="home-alg-btn tertiary" to="/binary-search">
+            <NavLink className="home-alg-btn" to="/binary-search">
               Binary Search
             </NavLink>
           </div>
         </div>
 
         <div className="home-section">
-          <h2 className="home-h2">Pathfinding Algorithms</h2>
+          <h2 className="home-h2">Pathfinding</h2>
           <div className="home-button-grid">
-            <NavLink className="home-alg-btn quaternary" to="/dfs">
+            <NavLink className="home-alg-btn" to="/dfs">
               Depth First Search
             </NavLink>
-            <NavLink className="home-alg-btn primary" to="/bfs">
+            <NavLink className="home-alg-btn" to="/bfs">
               Breadth First Search
             </NavLink>
           </div>
